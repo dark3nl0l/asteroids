965e0dba11a1f11ea80150f42cbaadde71d5c77a
simmonta <taylor_simmons@reyrey.com>
2025-08-27 18:21:09 -0500

    adding updates

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from player import *
+
 
 def main():
     pygame.init()
@@ -7,14 +9,25 @@ def main():
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(x, y)
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        dt = clock.tick(60) / 1000
+        updatable.update(dt)
         screen.fill("black")
+        for d in drawable:
+            d.draw(screen)
         pygame.display.flip()
-    
+
 
 if __name__ == "__main__":
     main()
